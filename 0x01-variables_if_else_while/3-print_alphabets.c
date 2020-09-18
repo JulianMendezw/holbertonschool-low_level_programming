192b28e23b9b1022a36e995f5041aa31decaeca9
julianmendezw <2358@holbertonschool.com>
2020-09-18 02:51:27 +0000

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+char d;
+
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+for (d = 'A' ; a <= 'Z' ; d++)
+{
+putchar(d)
+putchar('\n');
+}
+return (0);
+}
