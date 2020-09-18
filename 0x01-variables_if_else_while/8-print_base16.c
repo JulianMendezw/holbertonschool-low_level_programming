7379c31fec8d405d48a991a398dada5f8a076951
julianmendezw <2358@holbertonschool.com>
2020-09-18 04:27:23 +0000

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+int n;
+
+for (n = '0' ; n <= '9' ; n++)
+{
+putchar(n);
+}
+{
+for (c = 'a' ; c <= 'f' ; c++)
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
