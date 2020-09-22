b13d6bf0b749d3d5481141c3e32eab493ab26da2
Julianmendezw <2358@holbertonschool.com>
2020-09-22 16:09:17 -0500

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_last_digit - Entry point
+ *@n: integrer to check
+ *Return: Always 0 (Success)
+ *
+ */
+
+int print_last_digit(int n)
+{
+int d = n % 10;
+if (n == n)
+{
+return (d);
+}
+}
