3c5a78b611c609ee2ac56ed86b44cff84de25544
Julianmendezw <2358@holbertonschool.com>
2020-09-22 12:46:26 -0500

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_sign - Entry point
+ *@n:  character to check
+ *Return: Always 0 (Success)
+ *
+ */
+int print_sign(int n)
+{
+if (n > '0')
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+{
+_putchar('-');
+return (-1);
+}
+}
