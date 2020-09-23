a1ac9309a8a9b5c3d5ab1342e1c316bac5d02e53
Julianmendezw <2358@holbertonschool.com>
2020-09-22 23:15:27 -0500

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+*add - this to integrer
+*@a: number 1
+*@b: number 2
+*Return: Always 0 (Success)
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *times_table - Entry point
+ *integrer to check
+ *Return: Always 0 (Success)
+ *
+ */
+void times_table(void)
+{   
+    int n1, n2, m1;
+    for (n1 = 0; n1 < 10; n1++)
+    {
+        for (n2 = 0; n2 < 10; n2++)
+
+    _putchar('\n');
+    }
+   
+}
