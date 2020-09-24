9e080b4bd2cf8ddf5b7fa75323d48f784bafddde
Julianmendezw <2358@holbertonschool.com>
2020-09-24 16:17:47 -0500

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *mul - Entry point.
+ *@a: waiting for the character
+ *@b: second number
+ *Return: Always 0 (Success)
+ *
+ */
+int mul(int a, int b)
+{
+int r = (a * b);
+return (r);
+}
