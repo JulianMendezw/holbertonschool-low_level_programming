424ff18e8b9362720f854d77812ae260fb5b08aa
Julianmendezw <2358@holbertonschool.com>
2020-09-24 17:20:50 -0500

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - Entry point.
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+if (n != 2 || n != 4)
+{
+_putchar('0' + n);
+}
+}
+_putchar('\n');
+}
