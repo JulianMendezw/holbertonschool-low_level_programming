085952cf2207c7aa4edac1798adb21086c0d5e3d
Julianmendezw <2358@holbertonschool.com>
2020-09-24 13:33:51 -0500

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - Entry point.
+ *@c: waiting for the character
+ *Return: Always 0 (Success)
+ *
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
