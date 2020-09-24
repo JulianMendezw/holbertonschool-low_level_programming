89ec4b2a328c92ba5df85cb17dce4d7f6b22123b
Julianmendezw <2358@holbertonschool.com>
2020-09-24 13:19:25 -0500

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_isupper - Entry point.
+ *@c: waiting for the character
+ *Return: Always 0 (Success)
+ *
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
