0194036ebd70f58389fcb5c255ae15ab83ecb387
Julianmendezw <2358@holbertonschool.com>
2020-09-24 21:22:59 -0500

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *more_numbers - Entry point.
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+void more_numbers(void)
+{
+int n, m;
+
+for (m = 0; m < 10; m++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n >= 10)
+{
+_putchar((n / 10) + '0');
+}
+else
+{
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+}
