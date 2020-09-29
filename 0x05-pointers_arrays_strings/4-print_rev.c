ca6f751e5aa333ca299a27fda458d6c722deca5b
Julianmendezw <2358@holbertonschool.com>
2020-09-29 17:54:07 -0500

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_rev - counting the length of a string
+ *@s: value waited from the main.c
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+
+{
+int i;
+int x;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+for (x = i; x >= 0; x--)
+{
+_putchar(s[x]);
+}
+_putchar('\n');
+}
