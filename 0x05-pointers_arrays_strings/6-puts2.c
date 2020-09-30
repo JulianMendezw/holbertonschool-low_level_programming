220de9d91ccc515b787ec556d17e3918731a41f7
Julianmendezw <2358@holbertonschool.com>
2020-09-29 22:35:04 -0500

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - counting the length of a string
+ *@str: value waited from the main.c
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
