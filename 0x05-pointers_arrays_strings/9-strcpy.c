c36b2429880df4ac4bf403c45ad4c44a661d5610
Julianmendezw <2358@holbertonschool.com>
2020-09-30 23:56:36 -0500

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - counting the length of a string
+ *@dest: value waited from the main.c
+ *@src: root
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int c;
+
+for (c = 0; src[c] >= '\0'; c++)
+{
+dest[c] = src[c];
+}
+
+return (dest);
+}
+
