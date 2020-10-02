9f170724c3ddcf438168e9c33ff1a811ae975205
Julianmendezw <2358@holbertonschool.com>
2020-10-01 23:15:24 -0500

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,24 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * _strlen - counting the length of a string
- *@s: value waited from the main.c
+ * string_toupper - counting the length of a string
+ *
  *
  * Return: Always 0.
  */
+
+char *string_toupper(char * s)
+{
+	int i;
+       
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+			      
+		}
+	}
+	return (s);
+}
