ad6712673bf3009ef84fd52f08cc668c897a50da
Julianmendezw <2358@holbertonschool.com>
2020-10-01 23:18:13 -0500

    5.1

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,23 +2,22 @@
 
 /**
  * string_toupper - counting the length of a string
- *
+ *@s: string from main program
  *
  * Return: Always 0.
  */
 
-char *string_toupper(char * s)
+char *string_toupper(char *s)
 {
 	int i;
-       
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
-			      
 		}
 	}
 	return (s);
 }
+
