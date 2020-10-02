bd6042b05c02790116bfb69a8883ae0d92df6b9b
Julianmendezw <2358@holbertonschool.com>
2020-10-01 19:47:55 -0500

    3.1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int c;
-	int r;
 
 	for (c = 0; s1[c] != '\0' && s2[c] != '\0'; c++)
 	{
-		r = (s1[c] - s2[c]);
-		return (r);
+		return (s1[c] - s2[c]);
 	}
 	return (0);
 }
