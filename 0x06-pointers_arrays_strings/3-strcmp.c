5a752538cfba57bad5341f99d95f9e79c372b571
Julianmendezw <2358@holbertonschool.com>
2020-10-01 19:44:39 -0500

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - to compare two string
+ *@s1: value waited from the main.c
+ *@s2: source to be compare
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+	int r;
+
+	for (c = 0; s1[c] != '\0' && s2[c] != '\0'; c++)
+	{
+		r = (s1[c] - s2[c]);
+		return (r);
+	}
+	return (0);
+}
