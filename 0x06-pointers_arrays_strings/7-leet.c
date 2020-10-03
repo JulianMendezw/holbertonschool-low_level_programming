bb83aa255ab1cf8dc50144fd5c6e27f1d0509328
Julianmendezw <2358@holbertonschool.com>
2020-10-02 23:05:52 -0500

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,26 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * _strlen - counting the length of a string
+ * leet - counting the length of a string
  *@s: value waited from the main.c
  *
  * Return: Always 0.
  */
+char *leet(char *s)
+{
+	int i, x;
+	char a[] = "4433007711";
+	char l[] = "aAeEoOtTlL";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+		if (s[i] == l[x])
+		{
+			s[i] = a[x];
+		}
+		}
+	}
+	return (s);
+}
