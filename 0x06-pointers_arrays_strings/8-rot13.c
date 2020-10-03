02fda62da554d8b1d86ba4f2387f3021a1eb9eab
Julianmendezw <2358@holbertonschool.com>
2020-10-02 23:46:13 -0500

    8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,3 +7,23 @@
  *
  * Return: Always 0.
  */
+char *rot13(char *s)
+{
+	int i, x;
+	char a[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (s[i] == l[x])
+			{
+				s[i] = a[x];
+				break;	
+			}
+		}
+		
+	}
+	return (s);
+}
