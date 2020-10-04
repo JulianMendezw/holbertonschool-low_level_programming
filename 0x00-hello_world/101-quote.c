1c591e7cfec029600531d235349029c4ea8972ba
Julianmendezw <2358@holbertonschool.com>
2020-10-03 21:34:38 -0500

    9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+	
+}
+
+void _printf(char *s)
+{
+	int i;
+       
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+}
+
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	_printf(s);
 	return (1);
+	
 }
