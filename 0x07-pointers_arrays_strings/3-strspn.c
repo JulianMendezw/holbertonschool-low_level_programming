52b2cfa58be26a164ba635ece049f56ed31b7371
Julianmendezw <2358@holbertonschool.com>
2020-10-05 23:28:34 -0500

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strspn - prints buffer in hexa
+ * @s: the address of memory to print
+ * @accept: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, x;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				x++;
+				break;
+			}
+		}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+	}
+	return (x);
+}
