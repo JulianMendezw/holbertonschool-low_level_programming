54efbcaae139f7cca7ceaf27024da23410047ace
Julianmendezw <2358@holbertonschool.com>
2020-10-05 20:24:29 -0500

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the address of memory to print
+ * @c: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		return (s);
+	}
+	return (0);
+}
