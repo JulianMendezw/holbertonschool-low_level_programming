53752fa4c1cc1b938d99e494cb5dc1626b4e6a98
Julianmendezw <2358@holbertonschool.com>
2020-10-06 21:12:35 -0500

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strstr - is looking for a substring
+ * @haystack: the address of memory to print
+ * @needle: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; haystack; i++)
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i] == needle[j])
+		{
+			j++;
+			i++;
+		}
+		if (j > 0)
+		{
+			i -= j;
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+	return (0);
+}
