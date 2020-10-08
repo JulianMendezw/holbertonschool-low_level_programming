f184b8369d4fac67374a7ba17f428d7a8cab2a65
Julianmendezw <2358@holbertonschool.com>
2020-10-07 21:37:21 -0500

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * factorial - prints buffer in hexa
+ * @n: the address of memory to print
+ *
+ * Return: always return 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
