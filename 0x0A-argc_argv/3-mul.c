1112731d6307cd3c23adec3a987aa8a518cd36ed
Julian <2358@holbertonschool.com>
2020-10-11 01:03:06 -0500

    i moved the variables n1 and n2 inside else

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
 {
 	int n1 = 0, n2 = 0, result = 0;
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
 	if (arg != 3)
 	{
 		printf("Error\n");
@@ -20,6 +18,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		result = (n1 * n2);
 		printf("%d\n", result);
 	}
