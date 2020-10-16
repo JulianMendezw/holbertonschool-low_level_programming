bbf1f9009652a192fa2ab4c6e6a106de9fa3a5ed
Julian <2358@holbertonschool.com>
2020-10-15 22:30:12 -0500

    a function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0malloc_checked.c
+ * Auth: Julian Mendez w
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ *
+ *@b: Value unsigned received from main
+  *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}	
+	return (p);
+}
