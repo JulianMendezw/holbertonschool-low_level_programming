83ce44fef0d09bce061430ae01fb1de91c3f8e52
Julian <2358@holbertonschool.com>
2020-10-19 19:48:52 -0500

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct my_struct - Short description
+ * @a: First member
+ * @b: Second member
+ * @c: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
