8fdd81d342b694fb9b7fae4193e04ebffb864932
Julian <2358@holbertonschool.com>
2020-10-19 23:54:13 -0500

    updating idten

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,32 +16,32 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return(NULL);
-	}
-	if (d->name == NULL)
-	{
+		if (d->name == NULL)
+		{
 		printf("Name: (nil)\n");
-	}
+		}
 		else
 		{
 			printf("Name: %s\n", d->name);
 		}
-	if (d->age == 0)
-	{
+		if (d->age == 0)
+		{
 		printf("Age: (nil)\n");
-	}
+		}
 		else
 		{
 			printf("Age: %f\n", d->age);
 		}
-	if (d->owner == 0)
-	{
+		if (d->owner == 0)
+		{
 		printf("Owner: (nil)\n");
-	}
+		}
 		else
 		{
 			printf("Owner: %s\n", d->owner);
 		}
+
+	}
 }
