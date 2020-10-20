2420020a4fdf57e1e89dedb6ccce2b911596a9b6
Julian <2358@holbertonschool.com>
2020-10-20 00:17:03 -0500

    updating condition about age, always should be printed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,23 +20,16 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-		printf("Name: (nil)\n");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
 			printf("Name: %s\n", d->name);
 		}
-		if (d->age == 0)
-		{
-		printf("Age: (nil)\n");
-		}
-		else
-		{
-			printf("Age: %.6f\n", d->age);
-		}
+		printf("Age: %f\n", d->age);
 		if (d->owner == 0)
 		{
-		printf("Owner: (nil)\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
