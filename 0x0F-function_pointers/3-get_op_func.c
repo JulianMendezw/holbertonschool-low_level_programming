11636a775d2e6bb9f9180808055df8041bb57299
Julian <2358@holbertonschool.com>
2020-10-22 21:05:17 -0500

    formating windows

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-get_op_func.c
+ * Auth: Julian Mendez w
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ *  get_op_func - A function that searches for an integer.
+ *
+ *@s: String received from main.
+ *@int: num1 to be calculated.
+ *@int: num2 to be calculated.
+ * Return: void.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 3-main.c
+ * Auth: Julian Mendez w
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - A program that performs simple operations.
+ * Return: void.
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+}
