d2883d8cf147fad5d7b90d7d697b2318d796fceb
Julian <julianlx@hotmail.com>
2020-10-24 13:23:46 -0500

    adding NULL to the funtions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(strings, char *);
-		if (string)
+		if (string != NULL)
 		{
 			printf("%s", string);
 		}
@@ -33,7 +33,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if (separator && i < (n - 1))
+		if ((separator != NULL) && i < (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
