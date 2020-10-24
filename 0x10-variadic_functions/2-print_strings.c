0310fbe077ead38efa2ef4afc6f963d9bafc06cf
Julian <julianlx@hotmail.com>
2020-10-24 13:28:04 -0500

    changing order of va_list

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,16 +16,16 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list(strings);
 	unsigned int i;
 	char *string;
 
-	 va_list(strings);
 	 va_start(strings, n);
 
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(strings, char *);
-		if (string != NULL)
+		if (string)
 		{
 			printf("%s", string);
 		}
@@ -33,7 +33,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if ((separator != NULL) && i < (n - 1))
+		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
