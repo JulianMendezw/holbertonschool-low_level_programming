17958fda1d4509478dd64391e7977263ca6dac68
Julian <julianlx@hotmail.com>
2020-10-30 22:59:13 -0500

    a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,29 @@
+/*
+ * File: 4-free_list.c
+ * Auth: Julian Mendez w
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * free_list - a function that frees a list_t list.
+ *@head: pointer to the first node.
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
