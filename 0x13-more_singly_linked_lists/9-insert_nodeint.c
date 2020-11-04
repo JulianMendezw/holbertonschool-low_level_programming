eef325e6281224ff25264f0f6ad34abd2742e6dd
Julian <julianlx@hotmail.com>
2020-11-04 13:31:57 -0500

    adding line 24:25: if the list is emty return null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newn, *temp = *head;
 	unsigned int i;
 
+	if (!*head)
+		return(NULL);
+
 	for (i = 0; temp ; i++)
 	{
 		if (i != idx)
