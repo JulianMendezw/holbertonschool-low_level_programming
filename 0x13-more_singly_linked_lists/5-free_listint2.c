76bb26e6647a668380516c3efb9cd0863415c115
Julian <julianlx@hotmail.com>
2020-11-04 12:42:56 -0500

    freeding *head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -19,12 +19,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+	while (*head)
 	{
 		current = *head;
 		*head = (*head)->next;
 		free(current);
 	}
-	head = NULL;
-	free(head);
+	free(*head);
 }
