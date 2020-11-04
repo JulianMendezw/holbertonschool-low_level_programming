96457038af54f077ae74f71fd57a8f8c6b20fba5
Julian <julianlx@hotmail.com>
2020-11-04 14:10:57 -0500

    line 47: returning a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,6 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				newn->next = temp->next;
 				temp->next = newn;
+				return (newn);
 			}
 	}
 	return (NULL);
