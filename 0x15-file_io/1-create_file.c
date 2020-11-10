f0b29776e5be65ea239d54459c90e1263dd8877a
Julian <julianlx@hotmail.com>
2020-11-10 15:08:26 -0500

    updating a a for adn conditions if text_content exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,24 +23,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i, w;
+	int fd, i;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
 
-	for (i = 0; text_content[i]; i++)
+	for (i = 0; text_content[i] != 0; i++)
 	;
 
-	if (text_content == NULL)
-		w = write(fd, text_content, 0);
-	else
-		w = write(fd, text_content, i);
-
-		if (w == -1)
+	if (text_content)
+		if (write(fd, text_content, i) < i)
 			return (-1);
 
 	close(fd);
