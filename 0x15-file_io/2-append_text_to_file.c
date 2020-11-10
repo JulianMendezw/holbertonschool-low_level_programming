233e2d1aa02156d2910c4062637a3ff76cd86a06
Julian <julianlx@hotmail.com>
2020-11-10 17:39:57 -0500

    Fixing for: missing ;

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,8 +34,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (i = 0; text_content[i]; i++)
-			if (write(fd, text_content, i) < i)
-				return (-1);
+		;
+
+		if (write(fd, text_content, i) < i)
+			return (-1);
 	}
 
 	close(fd);
