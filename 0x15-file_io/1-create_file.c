13b1e7db4730468b5a119a46c025d38a645e3952
Julian <julianlx@hotmail.com>
2020-11-10 14:23:57 -0500

    changing order of flags of open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 		if (fd == -1)
 			return (-1);
 
@@ -39,8 +39,9 @@ int create_file(const char *filename, char *text_content)
 		w = write(fd, text_content, i);
 	else
 		w = write(fd, text_content, 0);
-	if (w == -1)
-		return (-1);
+
+		if (w == -1)
+			return (-1);
 
 	close(fd);
 	return (1);
