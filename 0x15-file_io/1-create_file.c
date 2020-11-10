e628703e2b591d98c33c9b5009890bcc738b4fb1
Julian <julianlx@hotmail.com>
2020-11-10 14:12:43 -0500

    adding else if text_content is empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,6 +40,11 @@ int create_file(const char *filename, char *text_content)
 		if (write(fd, text_content, i) < i)
 			return (-1);
 	}
+	else
+	{
+		if(write(fd, text_content, 0) < 0);
+			return (-1);
+	}
 
 	close(fd);
 	return (1);
