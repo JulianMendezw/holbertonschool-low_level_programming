4a0444d8a42f7153287f7b1ac7e121cc990cef45
Julian <julianlx@hotmail.com>
2020-11-09 21:34:52 -0500

    changing calloc for malloc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (fd == -1)
 			return (0);
 
-	file = calloc(letters, sizeof(char));
+	file = malloc(sizeof(char) * letters);
 		if (!file)
 			return (0);
 
