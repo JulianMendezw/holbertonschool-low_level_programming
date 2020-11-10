7a6d5d43dd9560790e729a4f4dc8beffa1e8f6d9
Julian <julianlx@hotmail.com>
2020-11-10 14:03:40 -0500

    changing control of for

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 
-	for (i = 0; text_content[i]; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 	;
 
 	if (text_content)
