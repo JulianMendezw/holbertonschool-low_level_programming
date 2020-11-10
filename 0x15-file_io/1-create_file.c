7a15830a7599a2286ac0117a966e134f2cdd7bbf
Julian <julianlx@hotmail.com>
2020-11-10 13:56:51 -0500

    changing condition on write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		if (write(fd, text_content, i) < 0)
+		if (write(fd, text_content, i) < i)
 			return (-1);
 	}
 
