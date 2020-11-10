0e20348b2930041fc1f0f240ac4a6b9bab0fc44b
Julian <julianlx@hotmail.com>
2020-11-10 13:32:51 -0500

    Telling to write where its should be write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,11 +29,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
 
-	if (fd == -1)
-		return (-1);
-
-	if (write(fd, &text_content, sizeof(text_content)) < 0)
+	if (write(STDIN_FILENO, &text_content, sizeof(text_content)) < 0)
 		return (-1);
 
 	close(fd);
