b50686ea0311b50aefa5e82137232730e38c1c31
Julian <julianlx@hotmail.com>
2020-11-10 12:50:37 -0500

    A function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+/*
+ * File: 0-read_textfile.c
+ * Auth: Julian Mendez w
+ */
+
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - A function that creates a file.
+ *
+ * @filename: file to read and write.
+ * @text_content: is a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	write(fd, &text_content, sizeof(text_content));
+
+	close(fd);
+	return (1);
+}
