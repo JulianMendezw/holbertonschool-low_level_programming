171ec4ad1c188f40298af575b2f94acf6d17096e
Julian <julianlx@hotmail.com>
2020-11-11 17:59:58 -0500

    Adding new output if open file_open fail

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -66,7 +66,11 @@ int copy_file(char *file_from, char *file_to)
 
 	f_from = open(file_from, O_RDONLY);
 		if (f_from == -1)
-			return (98);
+		{
+			free(text_copy);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
 
 	text_copy = malloc(sizeof(char) * BUF);
 		if (!text_copy)
