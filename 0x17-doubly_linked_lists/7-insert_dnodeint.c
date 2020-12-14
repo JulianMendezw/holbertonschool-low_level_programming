589027c0dd4c2c19018f213c7726655f05b2226f
Julian <julianlx@hotmail.com>
2020-12-14 13:11:42 -0500

    changing pointer pass to add_dnodeint function

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (idx == 0)
-		return (add_dnodeint(&head, n));
+		return (add_dnodeint(h, n));
 
 	node = malloc(sizeof(dlistint_t));
 	if (!node)
