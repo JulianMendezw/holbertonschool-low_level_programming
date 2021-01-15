3a6f421a48f50f231ab9bffde749203da783da95
Julian <julianlx@hotmail.com>
2021-01-15 13:16:44 -0500

    freeding

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index(((const unsigned char *)key), ht->size);
 
-	if (ht == NULL  || ht->array == NULL || key == NULL || value == NULL)
+	if (!ht  || !ht->array || !key || !value || strlen(key) == 0)
 		return (0);
 
 	if (ht->array[index])
@@ -29,8 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (!(strcmp(old_node->key, key)))
 			{
-				free(old_node->value);
-				old_node->value = strdup(value);
+				free(old_node->value), old_node->value = strdup(value);
 				return (1);
 			}
 			old_node = old_node->next;
@@ -40,23 +39,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 
 	if (!new_node->key || !new_node->value)
 	{
-		if (!new_node->key)
-			return (0);
-		free(new_node->key);
-			return (0);
+		if (new_node->key)
+			free(new_node->key);
+		free(new_node);
+		return (0);
 	}
-
 	if (ht->array[index])
 		new_node->next = ht->array[index];
-
 	ht->array[index] = new_node;
-
 	return (1);
 }
