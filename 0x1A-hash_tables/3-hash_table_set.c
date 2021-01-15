2992b6b78bbc53742e315e64ad4abaaacdef7076
Julian <julianlx@hotmail.com>
2021-01-15 13:42:34 -0500

    chanching validations

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *new_node, *old_node;
 
-	if (!ht  || !ht->array || !key || !value || strlen(key) == 0)
+	if (!ht  || !key || strlen(key) == 0)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
