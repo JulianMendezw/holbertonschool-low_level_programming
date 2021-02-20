0c71e8b0f373b31d27899019c647e4b269561ede
Julian <julianlx@hotmail.com>
2021-02-19 21:56:02 -0500

    Island perimter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Return the perimeter of the island """
+
+    perimeter = 0
+
+    for line in range(len(grid)):
+        for row in range(len(grid[line])):
+
+            if grid[line][row] == 1:
+
+                if grid[line][row - 1] == 0:  # Left
+                    perimeter = perimeter + 1
+
+                if grid[line][row + 1] == 0:  # Right
+                    perimeter = perimeter + 1
+
+                if grid[line - 1][row] == 0:  # Up
+                    perimeter = perimeter + 1
+
+                if grid[line + 1][row] == 0:  # Below
+                    perimeter = perimeter + 1
+
+    return perimeter
