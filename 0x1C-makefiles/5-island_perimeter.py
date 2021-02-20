5ec1b09a564dd7ba08fd0b32e36e34ead8b4d863
Julian <julianlx@hotmail.com>
2021-02-19 22:12:25 -0500

    adding new stament if the grid is greater than 100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,23 +8,24 @@ def island_perimeter(grid):
     perimeter = 0
     a = 0
 
-    if (len(grid[a])) <= 100 and len(grid) <= 100:
-        for line in range(len(grid)):
-            for row in range(len(grid[line])):
+    try:
+        if (len(grid[a])) <= 100 and len(grid) <= 100:
+            for line in range(len(grid)):
+                for row in range(len(grid[line])):
 
-                if grid[line][row] == 1:
+                    if grid[line][row] == 1:
 
-                    if grid[line][row - 1] or grid[line][row - 1] == 0:
-                        perimeter = perimeter + 1
+                        if grid[line][row - 1] == 0:  # Left
+                            perimeter = perimeter + 1
 
-                    if grid[line][row + 1] or grid[line][row + 1] == 0:
-                        perimeter = perimeter + 1
+                        if grid[line][row + 1] == 0:  # Right
+                            perimeter = perimeter + 1
 
-                    if grid[line - 1][row] or grid[line - 1][row] == 0:
-                        perimeter = perimeter + 1
+                        if grid[line - 1][row] == 0:  # Up
+                            perimeter = perimeter + 1
 
-                    if grid[line + 1][row] or grid[line + 1][row] == 0:
-                        perimeter = perimeter + 1
-    else:
+                        if grid[line + 1][row] == 0:  # Below
+                            perimeter = perimeter + 1
+        return perimeter
+    except BaseException:
         pass
-    return perimeter
